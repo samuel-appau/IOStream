6b2a5e911cd6e0e234f022e5c55a4233158c41e9
samuel-appau <42771319+samuel-appau@users.noreply.github.com>
2018-10-15 08:14:35 -0700

    Add files via upload

---
+++ A4.10677819.cpp.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+struct student{
+	int IDnumber;
+	string name;
+	char gender;
+	int age;
+	float score,averageage,averagescore;
+	std[i];
+	}
+int main(){
+	int i;
+	
+for(int i=0;i<5;i++)
+student[i]=i+1;
+
+
+	cout<<"Enter your name"<<endl;
+	cin>>student[i].name;
+	
+	
+	cout<<"Enter your IDnumber"<<endl;
+	cin>>student[i].IDnumber;
+	
+	cout<<"Enter your age"<<endl;
+	cin>>std[i]age;
+	
+	cout<<"Whats your gender "<<endl;
+	cin>>student[i].gender;
+	
+	cout<<"Enter your score"<<endl;
+	
+	cin>>student[i].score;
+	
+	if(score>80){
+		
+		cout<<"Your grade is A"<< endl;	
+	}
+	 else if (70<=score<=79){
+	 
+	 
+	 cout<<"Your grade is B"<<endl;
+	 }
+	 else if(60<=score<=70){
+	 cout<<"Your grade is C"<< endl;
+}
+	 
+	 else if(50<=score<=69){
+	 
+	 cout<<"Your grade is D"<<endl;
+            }     
+	 else if(40<=score<=49){
+	 
+	 cout<< "Your grade is E"<<endl;
+	 }
+	 else  
+	 
+	 cout<<"Your grade is F" <<endl;
+	 
+	 
+int main(int argc,char**argv){
+	
+	string data="";
+	ofstream is;
+	is.open("sammy.txt");
+	is<<data;
+	
+	cin>>data;
+	
+	return 0;
+	
+}
